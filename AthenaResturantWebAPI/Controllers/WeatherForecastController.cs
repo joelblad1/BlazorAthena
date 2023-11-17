886ed88e04feb67e5bc9723ad689075b489a78dd
Simon Karlsson <simon.kevlarsjal@gmail.com>
2023-11-17 14:00:10 +0100

    weather forecast example

--- AthenaResturantWebAPI/Controllers/WeatherForecastController.cs
+++ AthenaResturantWebAPI/Controllers/WeatherForecastController.cs
@@ -24,7 +24,8 @@ namespace AthenaResturantWebAPI.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
+                //TemperatureC = Random.Shared.Next(-20, 55),
+                TemperatureC = 999,
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
--- BlazorAthenaFrontend/Data/WeatherForecastService.cs
+++ BlazorAthenaFrontend/Data/WeatherForecastService.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 namespace BlazorAthenaFrontend.Data
 {
     public class WeatherForecastService
@@ -7,7 +9,7 @@ namespace BlazorAthenaFrontend.Data
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+        public Task<WeatherForecast[]> GetForecastAsyncBak(DateOnly startDate)
         {
             return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -15,6 +17,18 @@ namespace BlazorAthenaFrontend.Data
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             }).ToArray());
+        }
+
+        public async Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+        {
+            using var client = new HttpClient();
+            var response = await client.GetStreamAsync("https://localhost:7088/WeatherForecast"); //TODO hardcoded port?
+            var forecasts = await JsonSerializer.DeserializeAsync<IEnumerable<WeatherForecast>>(response, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+            return forecasts.ToArray();
+
         }
     }
 }
