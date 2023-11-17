71cde9d49c29c6a87dc50019010d56018685f232
Julia <derpette87@gmail.com>
2023-11-17 13:29:13 +0100

    FKs for Tables

---
+++ AthenaResturantWebAPI/Migrations/20231117122324_fkToOrderLine.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AthenaResturantWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class fkToOrderLine : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderLineID",
+                table: "Orders",
+                column: "OrderLineID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_OrderLines_OrderLineID",
+                table: "Orders",
+                column: "OrderLineID",
+                principalTable: "OrderLines",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_OrderLines_OrderLineID",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_OrderLineID",
+                table: "Orders");
+        }
+    }
+}
